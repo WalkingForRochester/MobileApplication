4ec92295c65bf8bd86c1aa042e483b3e581e3ea1
Joe Rogers <joerogers@users.noreply.github.com>
2024-05-24 10:09:15 -0700

    Fix logging

--- app/src/main/java/com/walkingforrochester/walkingforrochester/android/WalkingForRochesterApp.kt
+++ app/src/main/java/com/walkingforrochester/walkingforrochester/android/WalkingForRochesterApp.kt
@@ -2,29 +2,24 @@ package com.walkingforrochester.walkingforrochester.android
 
 import android.app.Application
 import com.google.android.gms.maps.MapsInitializer
-import com.google.android.gms.maps.OnMapsSdkInitializedCallback
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class WalkingForRochesterApp : Application(), OnMapsSdkInitializedCallback {
+class WalkingForRochesterApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        MapsInitializer.initialize(applicationContext, MapsInitializer.Renderer.LATEST, this)
-
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
-    }
 
-    override fun onMapsSdkInitialized(renderer: MapsInitializer.Renderer) {
-        when (renderer) {
-            MapsInitializer.Renderer.LEGACY -> Timber.d("The latest version of the renderer is used.")
-            MapsInitializer.Renderer.LATEST -> Timber.d("The legacy version of the renderer is used.")
+        MapsInitializer.initialize(applicationContext, MapsInitializer.Renderer.LATEST) {
+            when (it) {
+                MapsInitializer.Renderer.LATEST -> Timber.d("The latest version of the renderer is used.")
+                MapsInitializer.Renderer.LEGACY -> Timber.d("The legacy version of the renderer is used.")
+            }
         }
     }
-
-
 }

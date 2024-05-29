0b4fbad6159549543096b7c281e9e147384d2a42
Joe Rogers <joerogers@users.noreply.github.com>
2024-05-29 10:34:38 -0700

    Simplify WFRButton by creating a SocialButton for that logic.

--- app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/common/WFRButton.kt
+++ app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/common/WFRButton.kt
@@ -1,38 +1,47 @@
 package com.walkingforrochester.walkingforrochester.android.ui.composable.common
 
-import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import com.walkingforrochester.walkingforrochester.android.R
+import com.walkingforrochester.walkingforrochester.android.ui.theme.WalkingForRochesterTheme
 
 @Composable
 fun WFRButton(
-    modifier: Modifier = Modifier,
-    @DrawableRes icon: Int? = null,
+    onClick: () -> Unit,
     @StringRes label: Int,
+    modifier: Modifier = Modifier,
     buttonColor: Color = MaterialTheme.colorScheme.inverseSurface,
     labelColor: Color = MaterialTheme.colorScheme.inverseOnSurface,
-    onClick: () -> Unit,
     enabled: Boolean = true,
     loading: Boolean = false
 ) {
+    val defaultPadding = ButtonDefaults.ContentPadding
+    val contentPadding = PaddingValues(
+        start = defaultPadding.calculateStartPadding(LayoutDirection.Ltr) + 12.dp,
+        top = defaultPadding.calculateTopPadding() + 4.dp,
+        end = defaultPadding.calculateEndPadding(LayoutDirection.Ltr) + 12.dp,
+        bottom = defaultPadding.calculateBottomPadding() + 4.dp
+    )
+
     Button(
         onClick = { if (!loading) onClick() },
         colors = ButtonDefaults.buttonColors(
@@ -40,31 +49,44 @@ fun WFRButton(
             contentColor = labelColor
         ),
         enabled = enabled,
-        modifier = modifier.height(52.dp)
+        modifier = modifier,
+        contentPadding = contentPadding
+
     ) {
-        Box(contentAlignment = Alignment.Center) {
-            if (loading) {
-                CircularProgressIndicator(
-                    modifier.size(27.dp),
-                    color = labelColor
-                )
-            }
-            Row(
-                modifier = if (loading) Modifier.alpha(0f) else Modifier,
-                verticalAlignment = Alignment.CenterVertically
+        if (loading) {
+            Box(
+                contentAlignment = Alignment.Center
             ) {
-                if (icon != null) {
-                    Icon(
-                        painter = painterResource(id = icon),
-                        contentDescription = null,
-                        tint = Color.Unspecified,
-                        modifier = Modifier.size(24.dp)
-                    )
-                }
+                CircularProgressIndicator(
+                    modifier.size(ButtonDefaults.IconSize),
+                    color = LocalContentColor.current
+                )
                 Text(
                     text = stringResource(label),
-                    style = MaterialTheme.typography.labelLarge,
-                    modifier = Modifier.padding(horizontal = 8.dp)
+                    color = Color.Transparent
+                )
+            }
+        } else {
+            Text(
+                text = stringResource(label),
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun WFRButtonPreview() {
+    WalkingForRochesterTheme {
+        Surface {
+            Box(
+                Modifier.size(200.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                WFRButton(
+                    onClick = { /*TODO*/ },
+                    label = R.string.sign_in,
+                    loading = true
                 )
             }
         }

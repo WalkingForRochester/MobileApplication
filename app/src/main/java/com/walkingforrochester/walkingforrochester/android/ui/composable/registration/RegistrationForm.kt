735356458773529b35a4d6efe9befcee148f809a
Joe Rogers <joerogers@users.noreply.github.com>
2024-05-30 12:35:25 -0700

    Fix bug where phone visualization would crash if type illegal character

--- app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/registration/RegistrationForm.kt
+++ app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/registration/RegistrationForm.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
@@ -91,7 +92,7 @@ fun RegistrationForm(
                 keyboardType = KeyboardType.Phone,
                 imeAction = ImeAction.Next
             ),
-            visualTransformation = PhoneNumberVisualTransformation(),
+            visualTransformation = PhoneNumberVisualTransformation(LocalContext.current),
             validationError = uiState.phoneValidationMessage,
             validationErrorColor = Color.Red,
             clearFieldIconEnabled = true

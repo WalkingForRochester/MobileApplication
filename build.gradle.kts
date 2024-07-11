6e12215835d30ceca58b2b579574d90b65f1635c
Joe Rogers <joerogers@users.noreply.github.com>
2024-07-11 15:01:23 -0700

    Migrate plugins to toml config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,11 @@
-buildscript {
-    extra["compose_version"] = "1.4.1"
-    extra["accompanist_version"] = "0.30.1"
-    extra["lifecycle_version"] = "2.6.1"
-    extra["hilt_version"] = "2.45"
-    extra["retrofit_version"] = "2.9.0"
-    extra["moshi_version"] = "1.14.0"
-    extra["camerax_version"] = "1.2.2"
-}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("com.android.library") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt.android) apply false
+    alias(libs.plugins.maps.secrets.plugin) apply false
 }

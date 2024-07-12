2ab82ec93e4d64e38b761435538669882a018e7a
Joe Rogers <joerogers@users.noreply.github.com>
2024-07-12 12:47:25 -0700

    Update compose libraries and fix warnings

--- app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/logawalk/AddressSearchField.kt
+++ app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/logawalk/AddressSearchField.kt
@@ -6,11 +6,22 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -54,7 +65,10 @@ fun AddressSearchField(
                             false
                         )
                     }) {
-                        Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
                     }
                 },
                 trailingIcon = if (uiState.addressQuery.isNotEmpty()) {
@@ -95,7 +109,7 @@ fun AddressSearchField(
                                 overflow = TextOverflow.Ellipsis
                             )
                         }
-                        Divider()
+                        HorizontalDivider()
                     }
                 }
 

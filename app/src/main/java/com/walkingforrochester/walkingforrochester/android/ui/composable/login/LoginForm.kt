7e4eb337736cdda6b467d3cd612c700c17f2f228
Joe Rogers <joerogers@users.noreply.github.com>
2024-08-14 17:44:58 -0700

    Add autofill support to login/registration pages

--- app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/login/LoginForm.kt
+++ app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/login/LoginForm.kt
@@ -7,9 +7,13 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.autofill.AutofillType
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.password
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -17,8 +21,10 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.walkingforrochester.walkingforrochester.android.R
 import com.walkingforrochester.walkingforrochester.android.ui.composable.common.WFRTextField
+import com.walkingforrochester.walkingforrochester.android.ui.modifier.autofill
 import com.walkingforrochester.walkingforrochester.android.ui.state.LoginScreenState
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun LoginForm(
     modifier: Modifier = Modifier,
@@ -32,6 +38,10 @@ fun LoginForm(
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         WFRTextField(
+            modifier = Modifier.autofill(
+                autofillTypes = listOf(AutofillType.EmailAddress),
+                onFill = onEmailAddressValueChange
+            ),
             value = loginScreenState.emailAddress,
             onValueChange = onEmailAddressValueChange,
             labelRes = R.string.email_address,
@@ -44,6 +54,12 @@ fun LoginForm(
             clearFieldIconEnabled = true
         )
         WFRTextField(
+            modifier = Modifier
+                .autofill(
+                    autofillTypes = listOf(AutofillType.Password),
+                    onFill = onPasswordValueChange
+                )
+                .semantics { password() },
             value = loginScreenState.password,
             onValueChange = onPasswordValueChange,
             labelRes = R.string.password,

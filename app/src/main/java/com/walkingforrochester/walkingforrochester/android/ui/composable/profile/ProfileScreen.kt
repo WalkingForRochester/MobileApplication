1d3b625d921decce08ea2eaf67fef16c0fae72ee
Joe Rogers <joerogers@users.noreply.github.com>
2024-08-23 14:12:28 -0700

    clear credentials when logging out or deleting account

--- app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/profile/ProfileScreen.kt
+++ app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/profile/ProfileScreen.kt
@@ -17,11 +17,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.walkingforrochester.walkingforrochester.android.R
+import com.walkingforrochester.walkingforrochester.android.network.GoogleCredentialUtil
 import com.walkingforrochester.walkingforrochester.android.ui.composable.common.WFRButton
 import com.walkingforrochester.walkingforrochester.android.ui.composable.common.WFROutlinedButton
 import com.walkingforrochester.walkingforrochester.android.ui.state.ProfileScreenEvent
@@ -34,11 +36,18 @@ fun ProfileScreen(
     profileViewModel: ProfileViewModel = hiltViewModel(),
     onLogoutComplete: () -> Unit = {},
 ) {
+    val context = LocalContext.current
     LaunchedEffect(Unit) {
         profileViewModel.eventFlow.collect { event ->
             when (event) {
-                ProfileScreenEvent.Logout -> onLogoutComplete()
-                ProfileScreenEvent.AccountDeleted -> onLogoutComplete()
+                ProfileScreenEvent.Logout -> {
+                    GoogleCredentialUtil.performSignOut(context = context)
+                    onLogoutComplete()
+                }
+                ProfileScreenEvent.AccountDeleted -> {
+                    GoogleCredentialUtil.performSignOut(context = context)
+                    onLogoutComplete()
+                }
             }
         }
     }

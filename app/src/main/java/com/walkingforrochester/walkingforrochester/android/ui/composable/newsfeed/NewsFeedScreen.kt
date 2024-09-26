3aaf95a1c1d7c2344b59552900e9b815036e2cc8
Joe Rogers <joerogers@users.noreply.github.com>
2024-09-26 14:02:29 -0700

    Update newsfeed to sdk 35

--- app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/newsfeed/NewsFeedScreen.kt
+++ app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/newsfeed/NewsFeedScreen.kt
@@ -1,5 +1,8 @@
 package com.walkingforrochester.walkingforrochester.android.ui.composable.newsfeed
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -7,8 +10,11 @@ import com.walkingforrochester.walkingforrochester.android.ui.composable.common.
 import com.walkingforrochester.walkingforrochester.android.ui.theme.WalkingForRochesterTheme
 
 @Composable
-fun NewsFeedScreen(modifier: Modifier = Modifier) {
-    WFRWebView(modifier = modifier, url = "https://walkingforrochester.org/category/news/")
+fun NewsFeedScreen(modifier: Modifier = Modifier,
+                   contentPadding: PaddingValues = PaddingValues()) {
+    WFRWebView(
+        modifier = modifier.fillMaxSize().padding(contentPadding),
+        url = "https://walkingforrochester.org/category/news/")
 }
 
 @Preview(showBackground = true)

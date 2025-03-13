e4090caee99872951b6c73a5c47c5424d971648c
Joe Rogers <joerogers@users.noreply.github.com>
2025-03-13 16:57:59 -0700

    Update number picker

--- app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/common/HorizontalNumberPicker.kt
+++ app/src/main/java/com/walkingforrochester/walkingforrochester/android/ui/composable/common/HorizontalNumberPicker.kt
@@ -1,11 +1,13 @@
 package com.walkingforrochester.walkingforrochester.android.ui.composable.common
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.automirrored.filled.ArrowLeft
+import androidx.compose.material.icons.automirrored.filled.ArrowRight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -18,25 +20,31 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.walkingforrochester.walkingforrochester.android.R
 import com.walkingforrochester.walkingforrochester.android.ui.theme.WalkingForRochesterTheme
 
 @Composable
 fun HorizontalNumberPicker(
-    modifier: Modifier = Modifier,
     minValue: Int,
     maxValue: Int,
-    defaultValue: Int,
-    buttonSize: Dp = 48.dp,
-    textStyle: TextStyle = MaterialTheme.typography.titleLarge,
-    onValueChange: (Int) -> Unit
+    modifier: Modifier = Modifier,
+    defaultValue: Int = minValue,
+    buttonSize: Dp = 32.dp,
+    textStyle: TextStyle = MaterialTheme.typography.titleMedium,
+    onValueChange: (Int) -> Unit = {}
 ) {
     var currentValue by rememberSaveable { mutableIntStateOf(if (defaultValue > maxValue) maxValue else if (defaultValue < minValue) minValue else defaultValue) }
 
-    Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
+    Row(
+        modifier = modifier.widthIn(min = 120.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         IconButton(
             onClick = {
                 if (currentValue > minValue) {
@@ -46,13 +54,16 @@ fun HorizontalNumberPicker(
             enabled = currentValue > minValue
         ) {
             Icon(
-                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                imageVector = Icons.AutoMirrored.Default.ArrowLeft,
                 modifier = Modifier.size(buttonSize),
-                contentDescription = "Decrease number"
+                contentDescription = stringResource(R.string.decrease_value)
             )
         }
 
-        Text(text = "$currentValue", style = textStyle)
+        Text(
+            text = "$currentValue",
+            style = textStyle
+        )
 
         IconButton(
             onClick = {
@@ -63,9 +74,9 @@ fun HorizontalNumberPicker(
             enabled = currentValue < maxValue
         ) {
             Icon(
-                imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                imageVector = Icons.AutoMirrored.Default.ArrowRight,
                 modifier = Modifier.size(buttonSize),
-                contentDescription = "Increase number"
+                contentDescription = stringResource(R.string.increase_value)
             )
         }
     }
@@ -79,8 +90,8 @@ fun PreviewHorizontalNumberPicker() {
         Surface {
             HorizontalNumberPicker(
                 minValue = 0,
-                maxValue = 10,
-                defaultValue = 100,
+                maxValue = 88,
+                defaultValue = 78,
                 onValueChange = {})
         }
     }

03f784ea40184c8320ecce0b00ae02ccca870199
Joe Rogers <joerogers@users.noreply.github.com>
2025-04-15 14:13:24 -0700

    Add adaptive library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,6 +99,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive)
     implementation(libs.androidx.compose.material3.window.size)
     implementation(libs.androidx.compose.material.icons.extended)
 

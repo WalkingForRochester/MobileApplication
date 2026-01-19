8cdc4861d0622b56ff3794b95d521b978335979d
Joe Rogers <joerogers@users.noreply.github.com>
2026-01-19 15:26:41 -0800

    Bump to facebook graph version v24.0 since SDK no longer bumps

--- app/src/main/java/com/walkingforrochester/walkingforrochester/android/WalkingForRochesterApp.kt
+++ app/src/main/java/com/walkingforrochester/walkingforrochester/android/WalkingForRochesterApp.kt
@@ -1,6 +1,7 @@
 package com.walkingforrochester.walkingforrochester.android
 
 import android.app.Application
+import com.facebook.FacebookSdk
 import com.google.android.gms.maps.MapsInitializer
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
@@ -14,6 +15,11 @@ class WalkingForRochesterApp : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+
+        // Manually bump the graph api version
+        FacebookSdk.setGraphApiVersion("v24.0")
+        //FacebookSdk.setIsDebugEnabled(true)
+        //FacebookSdk.addLoggingBehavior(LoggingBehavior.REQUESTS)
 
         MapsInitializer.initialize(applicationContext, MapsInitializer.Renderer.LATEST) {
             when (it) {

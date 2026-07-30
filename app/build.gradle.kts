ea8d77c45b01c00ea2449fa7ed12762a4eb9b54d
Joe Rogers <joerogers@users.noreply.github.com>
2026-07-30 15:57:54 -0700

    Update to version 2.3.2 (31)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.walkingforrochester.walkingforrochester.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 30
-        versionName = "2.3.2"
+        versionCode = 31
+        versionName = "2.3.3"
 
         // For now only supporting English, so stripping out other languages
         // Will strip pseudoLocales en-rXA or ar-rXB as well if testing.
